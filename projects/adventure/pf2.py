409b1759c1fc8d084387d3c4383ea2805da4161d
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-08-27 11:02:41 -0400

    done

--- projects/adventure/pf2.py
+++ projects/adventure/pf2.py
@@ -1,10 +1,12 @@
-from typing import Set, Dict, List
+# breadth first path-finding. Go breadth first until you hit a dead end or you have no unvisited neighbors
+# do a breadth first search for your nearest unvisited neighbor; go to the first one you find
+# repeat until your search for an unvisited neighbor yeilds no paths
+from typing import List, Dict, Set
 
-from graph import Graph
-from util import Queue
 from room import Room
+from util import Queue
 
-def find_path(starting_room: Room):
+def find_path(starting_room: Room) -> List[str]:
     raw_path: List[Room] = [starting_room]
     visited: Set[Room] = set([starting_room])
     rooms_graph = build_graph(starting_room)
@@ -12,46 +14,65 @@ def find_path(starting_room: Room):
     shortest_routes: Dict[Room, Dict[Room, List[Room]]] = {
         room: get_shortest_routes(room) for room in rooms_graph
     }
+    # shortest_routes_sorted: Dict[Room, List[List[Room]]] = make_dict_of_sorted_paths_by_length(
+    #     shortest_routes,
+    # )
 
+    cur_room: Room = starting_room
     while len(rooms_set.difference(visited)) > 0:
-        next_target: Room = get_next_target(list(rooms_graph.keys()), shortest_routes, visited)
-        print(f'next target room is {next_target.id}')
-        path_to_next = shortest_routes[raw_path[-1]][next_target][1 : ]
-        raw_path.extend(path_to_next)
-        # print(f'visiting: {path_to_next}')
-        for room in path_to_next:
-            visited.add(room)
+        readable_path = [item.id for item in raw_path]
+        # print(f'cur_path: {readable_path}')
+        raw_path.extend(get_path_to_nearest_dead_end(cur_room, shortest_routes, visited))
+        cur_room = raw_path[-1]
+        visited.add(cur_room)
+        raw_path.extend(get_path_to_nearest_unvisited(cur_room, shortest_routes, visited))
+        cur_room = raw_path[-1]
+        visited.add(cur_room)
+
 
     directional_path: List[str] = convert_rooms_path_to_direcctional(raw_path)
 
     return directional_path
 
-def get_next_target(
-        rooms: List[Room],
+# def make_dict_of_sorted_paths_by_length(shortest_routes: Dict[Room, Dict[Room, List[Room]]]) -> Dict[Room, List[List[Room]]]:
+#     shortest_routes_sorted: Dict[Room, List[List[Room]]] = {}
+
+#     for starting_room, shortest_paths in shortest_routes.items():
+#         for item in shortest_paths.values():
+#             print(item)
+#         # shortest_routes_sorted[starting_room] = list(shortest_paths.values()).sort(key=len)
+#         # print(shortest_routes_sorted[starting_room])
+
+    # return shortest_routes_sorted
+
+def get_path_to_nearest_dead_end(
+        starting_room: Room,
         shortest_routes: Dict[Room, Dict[Room, List[Room]]],
         visited: Set[Room],
-):
-    room_unweighted_values: Dict[Room, float]
-    room_weighted_values: Dict[Room, float]
+) -> List[Room]:
+    for cur_path in shortest_routes[starting_room].values():
+        cur_visited = visited.copy().union(set(cur_path))
+        cur_room = cur_path[-1]
+        if cur_room not in cur_visited and is_dead_end(cur_room, cur_visited):
+            return cur_path[1 : ]
+    return []
 
-    room_unweighted_values = {
-        room: get_average_path_unweighted_value(room,
-                                                shortest_routes,
-                                                visited,
-                                               ) for room in rooms
-    }
+def is_dead_end(room: Room, visited: Set[Room]):
+    for direction in room.get_exits:
+        if room.get_room_in_direction(direction) not in visited:
+            return False
+    return True
 
-    room_weighted_values = {
-        room: get_average_weighted_path_value(room,
-                                              shortest_routes,
-                                              room_unweighted_values,
-                                              ) for room in rooms
-    }
-
-    # unvisited = {key: value for key, value in room_weighted_values.items() if value > 0}
-
-    # return max(unvisited, key=unvisited.get)
-    return max(room_weighted_values, key=room_unweighted_values.get)
+def get_path_to_nearest_unvisited(
+        starting_room: Room,
+        shortest_routes: Dict[Room, Dict[Room, List[Room]]],
+        visited: Set[Room]
+) -> List[Room]:
+    for cur_path in shortest_routes[starting_room].values():
+        cur_room = cur_path[-1]
+        if cur_room not in visited:
+            return cur_path[1 : ]
+    return []
 
 def get_shortest_routes(starting_room):
     shortest_routes: Dict[Room, List[Room]] = {}
@@ -62,7 +83,7 @@ def get_shortest_routes(starting_room):
     while need_to_visit.size() > 0:
         cur_path: List[Room] = need_to_visit.dequeue()
         cur_room = cur_path[-1]
-        shortest_routes[cur_room] = cur_path
+        shortest_routes[cur_room] = tuple(cur_path)
         visited.add(cur_room)
 
         for direction in cur_room.get_exits():
@@ -73,34 +94,6 @@ def get_shortest_routes(starting_room):
                 need_to_visit.enqueue(next_path)
 
     return shortest_routes
-
-def get_average_path_unweighted_value(
-        room: Room,
-        shortest_routes: Dict[Room, Dict[Room, List[Room]]],
-        visited: Set[Room],
-):
-    total = 0.0
-    paths = shortest_routes[room].values()
-
-    for path in paths:
-        for cur_room in path:
-            total += 1 if cur_room not in visited else 0
-
-    return total / len(paths)
-
-def get_average_weighted_path_value(
-        room: Room,
-        shortest_routes: Dict[Room, Dict[Room, List[Room]]],
-        room_unweighted_value: Dict[Room, float],
-):
-    total = 0.0
-    paths = shortest_routes[room].values()
-
-    for path in paths:
-        for cur_room in path:
-            total += room_unweighted_value[cur_room]
-
-    return total
 
 def convert_rooms_path_to_direcctional(raw_path: List[Room]):
     directional_path: List[str] = []
@@ -117,7 +110,6 @@ def convert_rooms_path_to_direcctional(raw_path: List[Room]):
             directional_path.append('w')
         room = next_room
     return directional_path
-        
 
 def build_graph(starting_room: Room) -> Dict[Room, List[Room]]:
     need_to_visit = Queue()
@@ -138,32 +130,3 @@ def build_graph(starting_room: Room) -> Dict[Room, List[Room]]:
                 need_to_visit.enqueue(room)
 
     return rooms_graph
-
-# def average_dist_to_all_rooms(starting_room: Room, rooms_graph: Dict[Room, List[Room]]):
-#     cur_depth = 0
-#     for room, neighbors in rooms_graph.items():
-#         pass
-
-# def get_average_shortest_path_len(starting_room: Room):
-#     need_to_visit = Queue()
-#     visited: Set[Room] = set()
-
-#     depth = 0
-#     unvisited_exit_found: False
-#     path_count = 0
-#     total_paths_depth = 0
-
-#     for exit_direction in starting_room.get_exits():
-#         target: Room = starting_room.get_room_in_direction(exit_direction)
-#         if target not in visited:
-#             unvisited_exit_found = True
-#             need_to_visit.enqueue(target)
-#     if not unvisited_exit_found:
-#         path_count += 1
-#         total_paths_depth += depth
-
-#     # do this after everthing has been reached
-#     room_scores
-
-# rank all the rooms that are the hardest to get to by finding the average shorests route length relative to all other rooms
-# find the shortest route to each unvisited room from where you are; get the average score per move for each path (visited rooms are worth 0)
